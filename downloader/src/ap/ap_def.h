3103aebe6d155d45fa7f231aae2e97d1faf6e2d5
leekilhun <pba7189@gmail.com>
2022-09-16 18:17:33 +0900

    jL 컨트럴러 프로젝트 추가

---
+++ downloader/src/ap/ap_def.h
@@ -0,0 +1,18 @@
+#ifndef AP_DEF_H_
+#define AP_DEF_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "hw.h"
+
+
+void apExit(void);
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif
---
+++ downloader/src/ap/include/cli_run.h
@@ -0,0 +1,19 @@
+#ifndef CLI_RUN_H_
+#define CLI_RUN_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+#include "ap_def.h"
+
+
+void cliRunInit(void);
+
+
+#ifdef __cplusplus
+ }
+#endif
+
+#endif
